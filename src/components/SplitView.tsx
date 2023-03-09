ed2af510ac77faf854ad0fb388036bbe6e647fcd
paulicz44 <stastnyp@seznam.cz>
2023-03-09 22:21:54 +0100

    images + redo splitter unctionality with css, texts.

--- src/components/SplitView.tsx
+++ src/components/SplitView.tsx
@@ -1,15 +1,20 @@
 import './SplitView.css'
+import { useEffect } from 'preact/hooks'
 
 function SplitView({ children }: any) {
+	useEffect(() => {
+		window.addEventListener('click', (e) => {
+			console.log(e)
+			window.document.body.style.setProperty('--splitter', e.clientX + 'px')
+		})
+		return () => {
+			// Optional: Any cleanup code
+		}
+	}, [])
 	return (
 		<>
-			<div class="main-wrapper">
-				<div class="main left">{children}</div>
-			</div>
+			<main class="main">{children}</main>
 			<div class="divider">x</div>
-			<div class="main-wrapper right">
-				<div class="main right">{children}</div>
-			</div>
 		</>
 	)
 }
